d2fb2960e4fdf1685aaeacdff7f5a9b37290c978
Jongseok Choi <js.choi.85@gmail.com>
2018-03-11 13:54:52 +0900

    bug fixed

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sh_conf=$HOME/.${s}rc
 function oh_my_profiles_main {
     git clone https://github.com/pwnartist/oh-my-profiles.git $install_dir
     echo "source $install_dir/init.profile" >> $sh_conf
-    source $sh_conf
+    source $install_dir/init.profile
 }
 
 oh_my_profiles_main
