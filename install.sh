85e0a279cc69bfaec6a702cd4c1689463143647b
Jongseok Choi <js.choi.85@gmail.com>
2018-08-10 07:33:06 +0900

    theme was added.

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ sh_conf=$HOME/.${s}rc
 
 function oh_my_profiles_main {
     git clone https://github.com/pwnartist/oh-my-profiles.git $install_dir
+    for f in $install_dir/zsh-theme/*
+    do
+        ln -s $f $HOME/.oh-my-zsh/themes/
+    done
     echo "source $install_dir/init.profile" >> $sh_conf
     source $install_dir/init.profile
 }
